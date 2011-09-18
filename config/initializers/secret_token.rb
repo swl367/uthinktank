3ca8f6e7269bbb44a3600b59b2869e4a2f13f900
kevinlee <kevinwonlee@utexas.edu>
2011-09-17 23:00:36 -0500

    first commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Uthinktank::Application.config.secret_token = 'b624d7568514a86d71a493566a340cc3ab5e6a16f13e3f6fe7ef3e3ac22f6001a45170f4bb0471fa0afbb304732c1c2df5ea7f3f9354bda0c3ac9eb3cff30402'
